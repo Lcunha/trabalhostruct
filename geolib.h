b1fa51b89cde506ce437d8f46bfb6c621c4f7cc5
Gabriel Silva <gabriel.ssilva@outlook.com>
2013-10-08 19:58:05 -0300

    Adicionadas as funcoes plotaLinha e criaLinha, e alterada a funcao atualizaTela para suportar as novas funcoes

--- geolib.h
+++ geolib.h
@@ -81,7 +81,7 @@ void desenhaTela(TTela tela) {
 }
 
 void criaPonto(TVPontos *pontos, int x, int y, char psimbolo) {
-    if (pontos->quantidade == 5) {
+    if (pontos->quantidade >= 5) {
         printf("Quantidade máxima de pontos já foi atingida!");
         return;
     }
@@ -95,16 +95,84 @@ void criaPonto(TVPontos *pontos, int x, int y, char psimbolo) {
     pontos->quantidade += 1;
 }
 
-void plotaPonto(TTela *tela, TPonto ponto, char psimbolo) {
+void criaLinha(TVLinhas *linhas, int x1, int y1, int x2, int y2, char psimbolo) {
+    if (linhas->quantidade >= 5) {
+        printf("Quantidade máxima de linhas já foi atingida!");
+        return;
+    }
+
+    TLinha linha;
+    linha.x1 = x1;
+    linha.y1 = y1;
+    linha.x2 = x2;
+    linha.y2 = y2;
+    linha.psimbolo = psimbolo;
+
+    linhas->elementos[linhas->quantidade] = linha;
+    linhas->quantidade += 1;
+}
+
+
+void plotaPonto(TTela *tela, TPonto ponto) {
     tela->grid[ponto.x][ponto.y] = ponto.psimbolo;
 }
 
+void plotaLinha(TTela *tela, TLinha linha) {
+    int x1 = linha.x1;
+    int x2 = linha.x2;
+    int y1 = linha.y1;
+    int y2 = linha.y2;
+    int nx, ny;
+    int m;
+    int direcao;
+    int fimx, fimy;
+
+    if (x1 > x2) {
+        nx = x2;
+        fimx = x1;
+    } else {
+        nx = x1;
+        fimx = x2;
+    }
+
+    if (y1 < y2) {
+        ny = y1;
+        fimy = y2;
+    } else {
+        ny = y2;
+        fimy = y1;
+    }
+
+    if (x1 == x2) {
+        m = y2-y1;
+    } else {
+        m = (y2-y1)/(x2-x1);
+    }
+
+    while (nx <= fimx) {
+        TPonto ponto;
+        ponto.x = nx;
+        ponto.y = ny;
+        ponto.psimbolo = linha.psimbolo;
+        plotaPonto(tela, ponto);
+
+        nx += 1;
+        printf("%d+%d ", ny, m);
+        ny += m;
+
+    }
+}
 
 void atualizaTela(TTela *tela, TVPontos pontos, TVLinhas linhas, TVQuadrados quadrados, TVTriangulos triangulos) {
     int i;
 
     for(i=0; i<pontos.quantidade; i++) {
         TPonto ponto = pontos.elementos[i];
-        plotaPonto(tela, pontos.elementos[i], pontos.elementos[i].psimbolo);
+        plotaPonto(tela, pontos.elementos[i]);
+    }
+
+    for(i=0; i<linhas.quantidade; i++) {
+        TLinha linha = linhas.elementos[i];
+        plotaLinha(tela, linhas.elementos[i]);
     }
 }
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
     TTela tela;
     int i, j;
 
+
     tela = criaTela();
 
     TVPontos pontos;
@@ -12,11 +13,16 @@ int main() {
     TVQuadrados quadrados;
     TVTriangulos triangulos;
 
+    // Atenção: Coordenada x diz respeito às linhas, e y às colunas
+    // Não é como no plano cartesiano!
+
     criaPonto(&pontos, 0, 0,'l');
     criaPonto(&pontos, 1, 1,'i');
     criaPonto(&pontos, 2, 2, 'n');
     criaPonto(&pontos, 3, 3, 'h');
     criaPonto(&pontos, 4, 4,'a');
+
+    criaLinha(&linhas, 12, 7, 15, 20, '*');
 
     atualizaTela(&tela, pontos, linhas, quadrados, triangulos);
     desenhaTela(tela);
